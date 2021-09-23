446dba48bcc3a7b8524c787eae4c85f1b0c9fc36
Stepan Sonsky <stepansonsky@gmail.com>
2021-09-24 01:15:47 +0200

    skip market chart cache

--- app/src/main/java/io/cryptem/app/binding/ImageViewBindings.kt
+++ app/src/main/java/io/cryptem/app/binding/ImageViewBindings.kt
@@ -3,15 +3,22 @@ package io.cryptem.app.binding
 import android.graphics.drawable.PictureDrawable
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.load.Key
+import com.bumptech.glide.signature.ObjectKey
 import io.cryptem.app.util.svg.GlideApp
 import io.cryptem.app.util.svg.SvgSoftwareLayerSetter
+import java.security.Signature
 
-@BindingAdapter("svgUrl")
-fun ImageView.bindSvg(url: String?) {
+@BindingAdapter(value = ["svgUrl", "skipCache"], requireAll = false)
+fun ImageView.bindSvg(url: String?, skipCache : Boolean = false) {
     if (url != null) {
-        val requestBuilder = GlideApp.with(context)
+        var requestBuilder = GlideApp.with(context)
             .`as`(PictureDrawable::class.java)
             .listener(SvgSoftwareLayerSetter())
+
+        if (skipCache){
+            requestBuilder = requestBuilder.skipMemoryCache(skipCache).signature(ObjectKey(System.currentTimeMillis()))
+        }
         requestBuilder.load(url).into(this)
     }
 }
