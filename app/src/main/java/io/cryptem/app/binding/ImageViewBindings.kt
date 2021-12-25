46aee38840f34e9051d717d313ce5b02fc975253
Stepan Sonsky <stepansonsky@gmail.com>
2021-12-26 00:25:51 +0100

    optimized market charts loading

--- app/src/main/java/io/cryptem/app/binding/ImageViewBindings.kt
+++ app/src/main/java/io/cryptem/app/binding/ImageViewBindings.kt
@@ -3,6 +3,7 @@ package io.cryptem.app.binding
 import android.graphics.drawable.PictureDrawable
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.signature.ObjectKey
 import io.cryptem.app.util.svg.GlideApp
 import io.cryptem.app.util.svg.SvgSoftwareLayerSetter
@@ -10,13 +11,10 @@ import io.cryptem.app.util.svg.SvgSoftwareLayerSetter
 @BindingAdapter(value = ["svgUrl", "skipCache"], requireAll = false)
 fun ImageView.bindSvg(url: String?, skipCache : Boolean = false) {
     if (url != null) {
-        var requestBuilder = GlideApp.with(context)
+        val requestBuilder = GlideApp.with(context)
             .`as`(PictureDrawable::class.java)
             .listener(SvgSoftwareLayerSetter())
 
-        if (skipCache){
-            requestBuilder = requestBuilder.skipMemoryCache(skipCache).signature(ObjectKey(System.currentTimeMillis()))
-        }
-        requestBuilder.load(url).into(this)
+        requestBuilder.load(url).diskCacheStrategy(DiskCacheStrategy.NONE).into(this)
     }
 }
